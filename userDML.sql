343ee509df6d201192df480ba94a9a60e7ca0856
(hyungkey) <(hyungkey0601@gmail.com)>
2025-06-09 09:45:14 +0900

    feat:유저로그인/회원가입dml 추가

--- userDML.sql
+++ userDML.sql
@@ -13,6 +13,21 @@ insert into user (email, password, nickname, role, profile_image, job) values
 
 -- 회원가입: 일반 유저
 insert into user (email, password, nickname, role, profile_image, job) values
-('charlie@naver.com', 'asdkj!as212', 'charlie', 'USER', 'https://image.site/bob.png', '개발자');
+('charlie@naver.com', 'hash_asdkj!as212', 'charlie', 'USER', 'https://image.site/bob.png', '개발자');
 
 select * from user where email = 'charlie@naver.com';
+
+
+-- 로그인: 사용자는 이메일, 비밀번호로 로그인한다.
+select * from user;
+
+select * from user 
+where email = 'charlie@naver.com' 
+	and password = 'hash_asdkj!as212';
+
+
+-- 회원 탈퇴: 사용자가 계정을 삭제한다.
+delete from user
+where user_id = 10;
+
+select * from user;
